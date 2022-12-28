72df391cba1f42f8caa932b732fa87c14363c825
Nadir Syed <nadir.syedsammut@gmail.com>
2022-12-28 23:00:58 +0100

    added strings

--- kmk_base/code.py
+++ kmk_base/code.py
@@ -1,11 +1,20 @@
 # RP2040 based basic KMK setup
-
+import board
 from kmk.modules.layers import Layers as _Layers
 from kmk.extensions.rgb import RGB, AnimationModes
 from kmk.keys import KC
-from rp2040 import RP2040
+from kmk.handlers.sequences import send_string
 
-import board
+STRINGS = [
+            send_string("NDR008 - PSX, Cars and Pizza!"),
+            send_string("PSX lives forever"),
+            send_string("PCSX-Redux is a cool emulator"),
+            send_string("GRRRR"),
+            send_string("That's cool thanks!"),
+            send_string("That is awesome!")
+        ]
+
+from rp2040 import RP2040
 
 
 keyboard = RP2040()
@@ -17,13 +26,12 @@ keyboard.extensions.append(rgb)
 
 class Layers(_Layers):
     last_top_layer = 0
-    hues = ((255, 0, 0), (0, 0, 255))
+    hues = ((255, 0, 0), (0, 0, 255), (0, 255, 0))
 
     def after_hid_send(self, keyboard):
         if keyboard.active_layers[0] != self.last_top_layer:
             self.last_top_layer = keyboard.active_layers[0]
             rgb.set_rgb_fill(self.hues[self.last_top_layer])
-
 
 keyboard.modules.append(Layers())
 keyboard.keymap = [
@@ -31,14 +39,19 @@ keyboard.keymap = [
     [
         KC.F13, KC.F16, KC.F18,
         KC.F14, KC.F17, KC.F19,
-        KC.F15, KC.TG(1), KC.F20,
+        KC.F15, KC.TO(1), KC.F20,
     ],
 
     # Function Layer
     [
         KC.Q,  KC.W,  KC.T,
-        KC.A,  KC.S,  KC.D,
-        KC.X, KC.TRNS, KC.O,
+        KC.A,  KC.S,  KC.N,
+        KC.LEFT, KC.TO(2), KC.RIGHT,
+    ],
+    [
+        STRINGS[0],  STRINGS[1],  STRINGS[2],
+        STRINGS[3],  STRINGS[4],  STRINGS[5],
+        KC.LEFT, KC.TO(0), KC.RIGHT,
     ],
 ]
 # fmt: on
